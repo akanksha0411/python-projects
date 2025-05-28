796f9f97c442b134cd7a9bba6a9ab2bb9d83a7db
Akanksha <akankshasukhramani@gmail.com>
2025-05-28 14:32:47 +0530

    Update polygon_generator.py

--- day18-19/turtle-games/polygon_generator.py
+++ day18-19/turtle-games/polygon_generator.py
@@ -4,7 +4,7 @@ import random
 tim = t.Turtle()
 t.colormode(255)
 
-colors_list = ["dark orange", "chartreuse", "magenta", "medium purple", "brown", "deep sky blue", "slate blue", "red","orange","yellow","green","blue","indigo","purple"]
+# colors_list = ["dark orange", "chartreuse", "magenta", "medium purple", "brown", "deep sky blue", "slate blue", "red","orange","yellow","green","blue","indigo","purple"]
 n = 3
 for _ in range(7):
     tim.pencolor((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))
